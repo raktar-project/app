42ea1f5d0b1902ced386848161f7d685c27a2a1a
David Steiner <david_j_steiner@yahoo.co.nz>
2023-05-01 22:29:29 +0100

    Render markdowns using react-markdown

--- src/components/Crate.tsx
+++ src/components/Crate.tsx
@@ -2,7 +2,11 @@ import { FC } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "urql";
 import { CrateDetailsDocument } from "../generated/graphql.ts";
-import { LinearProgress } from "@mui/material";
+import { LinearProgress, Paper, Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import Grid from "@mui/material/Grid";
 
 const Crate: FC = () => {
   const { crateName } = useParams();
@@ -10,7 +14,18 @@ const Crate: FC = () => {
   if (crateName === undefined) {
     return <div>Missing crate name!</div>;
   } else {
-    return <CrateDetails name={crateName} />;
+    return (
+      <Grid container sx={{ marginTop: 3 }}>
+        <Grid item xs={2} />
+        <Grid item xs={6}>
+          <CrateDetails name={crateName} />
+        </Grid>
+        <Grid item xs={2}>
+          <Typography variant="h5">This will be the sidebar</Typography>
+        </Grid>
+        <Grid item xs={2} />
+      </Grid>
+    );
   }
 };
 
@@ -25,7 +40,15 @@ const CrateDetails: FC<{ name: string }> = ({ name }) => {
   } else if (fetching) {
     return <LinearProgress />;
   } else if (data) {
-    return <div>{data.crateDetails.name}</div>;
+    return (
+      <Box>
+        {data.crateDetails.readme && (
+          <Paper sx={{ padding: 2 }}>
+            <ReactMarkdown children={data.crateDetails.readme} remarkPlugins={[remarkGfm]} />
+          </Paper>
+        )}
+      </Box>
+    );
   }
 
   return <div>Unexpected state.</div>;
