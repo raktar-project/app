720bab6b2704539b075ec08bfd1fa7f587971d61
David Steiner <david_j_steiner@yahoo.co.nz>
2023-05-01 08:08:41 +0100

    Nicer display of generated tokens

--- src/components/TokenGenerator.tsx
+++ src/components/TokenGenerator.tsx
@@ -1,5 +1,15 @@
 import { ChangeEvent, FC, useState } from "react";
-import { Box, Button, LinearProgress, TextField, Typography } from "@mui/material";
+import {
+  Button,
+  LinearProgress,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TextField,
+} from "@mui/material";
 import { useMutation } from "urql";
 import { GenerateTokenDocument } from "../generated/graphql.ts";
 
@@ -7,33 +17,54 @@ const TokenGenerator: FC = () => {
   const [tokenName, setTokenName] = useState<string>("");
   const [{ data, error, fetching }, generateToken] = useMutation(GenerateTokenDocument);
 
-  if (data) {
-    return <div>{`Token: ${data.generateToken.token}`}</div>;
-  } else if (error) {
+  if (error) {
     return <div>{`Failed to get token: ${error.message}`}</div>;
   } else if (fetching) {
     return <LinearProgress />;
   }
 
   return (
-    <Box>
-      <Typography variant="h5">New Token</Typography>
-      <TextField
-        onChange={(event: ChangeEvent<HTMLInputElement>) => {
-          setTokenName(event.target.value);
-        }}
-        fullWidth
-        size="small"
-      />
-      <Button
-        onClick={() => generateToken({ name: tokenName })}
-        fullWidth
-        variant="outlined"
-        sx={{ marginTop: 2 }}
-      >
-        Generate New Token
-      </Button>
-    </Box>
+    <Paper elevation={3} sx={{ padding: 2 }}>
+      {!data && (
+        <>
+          <TextField
+            onChange={(event: ChangeEvent<HTMLInputElement>) => {
+              setTokenName(event.target.value);
+            }}
+            label="Token name"
+            fullWidth
+            size="small"
+            disabled={data !== undefined}
+          />
+          <Button
+            onClick={() => generateToken({ name: tokenName })}
+            fullWidth
+            variant="outlined"
+            sx={{ marginTop: 2 }}
+          >
+            Generate New Token
+          </Button>
+        </>
+      )}
+      {data && (
+        <>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Secure Token</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell sx={{ width: "100" }}>{tokenName}</TableCell>
+                <TableCell>{data.generateToken.token}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </>
+      )}
+    </Paper>
   );
 };
 
