e122e37ca02a02096a64b7871587c482567d5a6c
David Steiner <david_j_steiner@yahoo.co.nz>
2023-06-20 19:21:11 +0200

    Make hosted zone domain configurable rather than app URL

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -1,3 +1,5 @@
+const appUrl = `https://crates.${import.meta.env.VITE_HOSTED_ZONE_DOMAIN}`;
+
 export default {
   Auth: {
     region: import.meta.env.VITE_AWS_REGION,
@@ -5,8 +7,8 @@ export default {
     userPoolWebClientId: import.meta.env.VITE_AWS_USER_POOL_CLIENT_ID,
     authenticationFlowType: "USER_SRP_AUTH",
     oauth: {
-      redirectSignIn: `https://${import.meta.env.VITE_APP_DOMAIN}/cb`,
-      redirectSignOut: `https://${import.meta.env.VITE_APP_DOMAIN}`,
+      redirectSignIn: `${appUrl}/cb`,
+      redirectSignOut: appUrl,
       domain: import.meta.env.VITE_COGNITO_DOMAIN,
       responseType: "code",
     },
